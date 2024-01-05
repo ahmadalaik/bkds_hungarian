8abf4fe2726b5e3184c79f80dea91a610a38c4a7
ahmadalaik <ahmadalaik.mu@gmail.com>
2024-01-05 14:05:46 +0700

    changes load model

--- model.py
+++ model.py
@@ -86,7 +86,7 @@ y = df_clean['target']
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 
-model = pickle.load(open("model/knn_model", "rb"))
+model = pickle.load(open("model/knn_model.pkl", "rb"))
 
 y_pred = model.predict(X)
 accuracy = round((accuracy_score(y, y_pred)*100), 2)
